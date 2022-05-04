746b7f3f07dd9a6817a54b0923e4b4f3925070de
GaNardelli <gv.nardelli@gmail.com>
2022-05-03 21:10:09 -0300

    creating app

---
+++ reactAlura/ceep/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import FormularioCadastro from "./components/FormularioCadastro.jsx";
+import ListaDeNotas from "./components/ListaDeNotas.jsx";
+
+function App() {
+  return (
+    <section>
+      <FormularioCadastro />
+      <ListaDeNotas />
+    </section>
+  );
+}
+
+export default App;
