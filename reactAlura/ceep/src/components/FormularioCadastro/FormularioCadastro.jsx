22835e8e641f855e37b95c2f36ed3416717de5f4
GaNardelli <gv.nardelli@gmail.com>
2022-05-12 21:20:40 -0300

    20220512

--- reactAlura/ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ reactAlura/ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,18 +1,42 @@
 import React, { Component } from "react";
 import "./estilo.css";
 class FormularioCadastro extends Component {
+  constructor(props) {
+    super();
+    this.titulo = "";
+    this.nota = "";
+  }
+
+  _handlerMudancaDeTitulo(evento) {
+    evento.stopPropagation();
+    this.titulo = evento.target.value;
+  }
+
+  _handlerMudancaTexto(evento) {
+    evento.stopPropagation();
+    this.nota = evento.target.value;
+  }
+
+  _criarCard(evento) {
+    evento.preventDefault();
+    evento.stopPropagation();
+    this.props.criarNota(this.titulo, this.nota);
+  }
+
   render() {
     return (
-      <form className="form-cadastro ">
+      <form className="form-cadastro " onSubmit={this._criarCard.bind(this)}>
         <input
           type="text"
           placeholder="Título"
           className="form-cadastro_input"
+          onChange={this._handlerMudancaDeTitulo.bind(this)}
         />
         <textarea
           rows={15}
           placeholder="Escreva sua nota..."
           className="form-cadastro_input"
+          onChange={this._handlerMudancaTexto.bind(this)}
         />
         <button className="form-cadastro_input form-cadastro_submit">
           Criar Nota
