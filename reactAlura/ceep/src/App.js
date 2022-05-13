25ba5fb44beef97cca829d03f0913b21849651f6
GaNardelli <gv.nardelli@gmail.com>
2022-05-12 21:55:44 -0300

    ciclo de vida das componentes

--- reactAlura/ceep/src/App.js
+++ reactAlura/ceep/src/App.js
@@ -4,28 +4,33 @@ import FormularioCadastro from "./components/FormularioCadastro";
 import "./assets/App.css";
 import './assets/index.css';
 class App extends Component {
-  constructor() {
+
+  constructor(){
     super();
-    this.notas = [];
+
+    this.state = {
+      notas:[]
+    }
   }
 
-  criarNota(titulo, texto) {
-    const novaNota = {
-      titulo,
-      texto,
-    };
-
-    this.notas.push(novaNota);
+  criarNota(titulo, texto){
+    const novaNota = {titulo, texto};
+    const novoArrayNotas = [...this.state.notas,novaNota]
+    const novoEstado = {
+      notas:novoArrayNotas
+    }
+    this.setState(novoEstado)
   }
 
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
-        <ListaDeNotas notas={this.notas} />
+        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+        <ListaDeNotas notas={this.state.notas}/>
       </section>
     );
   }
 }
 
+//new ListaDeNotas({notas:this.notas})
 export default App;
--- reactAlura/ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ reactAlura/ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,42 +1,46 @@
 import React, { Component } from "react";
 import "./estilo.css";
 class FormularioCadastro extends Component {
-  constructor(props) {
-    super();
-    this.titulo = "";
-    this.nota = "";
+
+  constructor(props){
+    super(props);
+    this.titulo ="";
+    this.texto ="";
   }
 
-  _handlerMudancaDeTitulo(evento) {
+  _handleMudancaTitulo(evento){
     evento.stopPropagation();
     this.titulo = evento.target.value;
   }
 
-  _handlerMudancaTexto(evento) {
+  _handleMudancaTexto(evento){
     evento.stopPropagation();
-    this.nota = evento.target.value;
+    this.texto = evento.target.value;
   }
 
-  _criarCard(evento) {
+  _criarNota(evento){
     evento.preventDefault();
     evento.stopPropagation();
-    this.props.criarNota(this.titulo, this.nota);
+    this.props.criarNota(this.titulo, this.texto);
+    
   }
 
   render() {
     return (
-      <form className="form-cadastro " onSubmit={this._criarCard.bind(this)}>
+      <form className="form-cadastro"
+        onSubmit={this._criarNota.bind(this)}
+      >
         <input
           type="text"
           placeholder="Título"
           className="form-cadastro_input"
-          onChange={this._handlerMudancaDeTitulo.bind(this)}
+          onChange={this._handleMudancaTitulo.bind(this)}
         />
         <textarea
           rows={15}
           placeholder="Escreva sua nota..."
           className="form-cadastro_input"
-          onChange={this._handlerMudancaTexto.bind(this)}
+          onChange={this._handleMudancaTexto.bind(this)}
         />
         <button className="form-cadastro_input form-cadastro_submit">
           Criar Nota
