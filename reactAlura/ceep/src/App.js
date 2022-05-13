9210240718d44185d91259e8ac7aca69899dadc2
GaNardelli <gv.nardelli@gmail.com>
2022-05-12 22:31:24 -0300

    20220512CicloDeVidaDasComponentes

--- reactAlura/ceep/src/App.js
+++ reactAlura/ceep/src/App.js
@@ -4,29 +4,42 @@ import FormularioCadastro from "./components/FormularioCadastro";
 import "./assets/App.css";
 import './assets/index.css';
 class App extends Component {
-
-  constructor(){
+  constructor() {
     super();
 
     this.state = {
-      notas:[]
-    }
+      notas: [],
+    };
   }
 
-  criarNota(titulo, texto){
-    const novaNota = {titulo, texto};
-    const novoArrayNotas = [...this.state.notas,novaNota]
+  criarNota(titulo, texto) {
+    const novaNota = { titulo, texto };
+    const novoArrayNotas = [...this.state.notas, novaNota];
     const novoEstado = {
-      notas:novoArrayNotas
-    }
-    this.setState(novoEstado)
+      notas: novoArrayNotas,
+    };
+    this.setState(novoEstado);
+  }
+
+  deletarNota(index) {
+    let arrayNotas = this.state.notas;
+    arrayNotas.splice(index, 1);
+
+    this.setState({
+      notas: arrayNotas,
+    });
+
+    console.log("deletar");
   }
 
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
-        <ListaDeNotas notas={this.state.notas}/>
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <ListaDeNotas
+          deletarNota={this.deletarNota.bind(this)}
+          notas={this.state.notas}
+        />
       </section>
     );
   }
